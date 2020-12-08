ff777ae1d80e6d7f745f21e819c6b961d098835d
Edwin <edwin.celini@gmail.com>
2020-12-08 13:47:49 +0100

    style: Change MaterialCard.jsx to HomeCard.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,26 @@
 // React
-import React, { Component   } from "react";
+import React, { Component } from "react";
 // Material UI
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 // Components
-import MaterialCard from "../components/MaterialCard";
+import HomeCard from "../components/HomeCard";
 
 class Home extends Component {
   render() {
     return (
       <Grid container spacing={3}>
-        <Grid item xs={12} md={6}><MaterialCard name="Heart" emoji="❤️" link="/heart"/></Grid>
-        <Grid item xs={12} md={6}><MaterialCard name="Flowers" emoji="💐" link="/"/></Grid>
-        <Grid item xs={12} md={6}><MaterialCard name="Sentences" emoji="💬" link="/"/></Grid>
-        <Grid item xs={12} md={6}><MaterialCard name="Playlists" emoji="🎵" link="/"/></Grid>
+        <Grid item xs={12} md={6}>
+          <HomeCard name="Heart" emoji="❤️" link="/heart" />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <HomeCard name="Flowers" emoji="💐" link="/" />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <HomeCard name="Sentences" emoji="💬" link="/" />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <HomeCard name="Playlists" emoji="🎵" link="/playlists" />
+        </Grid>
       </Grid>
     );
   }
